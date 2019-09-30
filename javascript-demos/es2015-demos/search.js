aa886ed5d3a13e09021c4682837782322172d1ca
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2019-09-30 16:49:19 +0530

    day#1-update#5

--- javascript-demos/es2015-demos/search.js
+++ javascript-demos/es2015-demos/search.js
@@ -1,7 +1,5 @@
 
-
 let numbers=[2,3,9,11,36,15,4,12,23,29,17,35,72,73,97,58];
-
 function isPrime(number){
     if (number<2) return false;
     for(let i = 2 ;i<number;i++)
@@ -10,13 +8,22 @@ function isPrime(number){
 
     return true;
 }
-
 function searchPrimes(numbers){
+    let result=[];
+    for (let number of numbers)
+        if (isPrime(number))
+            result.push(number)
+
+    return result;
+}
+function searchOdds(numbers){
 
 }
 
 let primes=searchPrimes(numbers);
-
 console.log('primes',primes);
+
+let odds=searchOdds(numbers);
+console.log('odds',odds);
 
 
---
+++ javascript-demos/es2015-demos/search2.js
@@ -0,0 +1,67 @@
+
+class Book{
+    constructor(title,author,rating,price){
+        this.title=title;
+        this.author=author;
+        this.price=price;
+        this.rating=rating;
+    }
+
+    toString(){
+        return `${this.price}\t${this.rating}\t${this.title}\t${this.author}`;
+    }
+}
+
+class BookStore{
+    
+    constructor(){
+        this.books=[];
+    }
+
+    getAll(){
+        return this.books;
+    }
+
+    getByTitle(title){
+        for(let book of books)
+            if(book.title===title)
+                return book;
+
+        return null;
+    }
+
+    addBook(book){
+        this.books.push(book);
+    }
+}
+
+
+function getDummyBookStore(){
+    let store=new BookStore();
+    store.addBook(new Book('The Count of Monte Cristo','Alexandre Dumas',4.8,250));
+    store.addBook(new Book('The Brethren','John Grisham',4.1,320));
+    store.addBook(new Book('Sons of Fortune','Jeffrey Archer',4.3,350));
+    store.addBook(new Book('Rashmirathi','Ramdhari Singh Dinkar',4.9,120));
+    store.addBook(new Book('Kane and Abel','Jeffrey Archer',4.5,280));
+    store.addBook(new Book('Kurukshetra','Ramdhari Singh Dinkar',4.6,170));
+
+    return store;
+}
+
+
+function printBooks(header,books){
+    console.log('header');
+    console.log('Price\tRating\tTitle\t\tAuthor')
+    for(let book of books){
+        console.log(`${book}`);
+    }
+    console.log('---');
+}
+
+
+//---------- main program begins here
+
+let store=getDummyBookStore();
+
+printBooks('All books', store.getAll());
+
